5d68ca96e90895deb0c402aedfadcfa1fcf51d8f
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2022-05-13 14:56:04 +0200

    ad cors to server

--- back/server.js
+++ back/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+
 
 const app = express()
 
@@ -11,7 +13,7 @@ db.on('error', (error) => {console.log(error)})
 db.once('open', () => console.log('Connectet to DB'))
 
 app.use(express.json())
-
+app.use(cors())
 
 app.use('/tasks', require('./routes/task'))
 
